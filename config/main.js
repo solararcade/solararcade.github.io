2b962fcf37ecafa5f3eefcae30b5f0014068ce6c
totallynotharry <162619834+totallynotharry@users.noreply.github.com>
2026-01-22 15:30:43 +0000

    Update main.js

--- config/main.js
+++ config/main.js
@@ -1,7 +1,7 @@
 // This changes the title of your site
 
 var sitename = "solar."; // Change this to change the name of your website.
-var subtext = "a fix has been released - games may now work!"; // set the subtext
+var subtext = "created by harry z - support from seb and jake"; // set the subtext
 
 // more settings in main.css
 
