1ee9c56b44adbaeaed0b7754d01cbb7c79e3ad69
totallynotharry <162619834+totallynotharry@users.noreply.github.com>
2026-02-10 16:37:38 +0000

    Update subtext for the website title

--- config/main.js
+++ config/main.js
@@ -1,7 +1,7 @@
 // This changes the title of your site
 
 var sitename = "solar."; // Change this to change the name of your website.
-var subtext = "i'm mc'lovin it"; // set the subtext
+var subtext = "WE ARE BACK BABAAAAAAY!"; // set the subtext
 
 // more settings in main.css
 
